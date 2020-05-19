9965e695e90a46ca4943cb044be0244cb34db19a
authetic <1019892846@qq.com>
2020-05-19 18:27:15 +0800

    Add city-search-logic

--- src/components/city/search.vue
+++ src/components/city/search.vue
@@ -1,12 +1,74 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input
+        v-model="keyword"
+        class="search-input"
+        type="text" placeholder="输入城市名或拼音"
+        @focus="focused=true"
+        @blur="focused=false"
+      >
+    </div>
+    <div class="search-content" ref="search" v-show="keyword || focused">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from '@better-scroll/core'
+
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null,
+      focused: false
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (const i in this.cities) {
+          this.cities[i].forEach(item => {
+            if (item.spell.indexOf(this.keyword) !== -1 ||
+                item.name.indexOf(this.keyword) !== -1) {
+                result.push(item)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -25,4 +87,17 @@ export default {
       border-radius .06rem
       color #666
       padding 0 .1rem
+  .search-content
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    z-index 1
+    background #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      background #fff
 </style>
